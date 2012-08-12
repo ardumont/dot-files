3c59213ffe92cd754f8ae655a1ffc6360e1cbbf7
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-08-12 17:32:26 +0200

    Script for leiningen2 to deploy in ~/.lein

---
+++ profiles.clj
@@ -0,0 +1,14 @@
+{:user
+ {:plugins [[lein-swank "1.4.4"]
+            [lein-cljs "0.2.2"]
+            [lein-difftest "1.3.7"]
+            [lein-marginalia "0.7.1"]
+            [lein-pprint "1.1.1"]
+            [lein-midje "2.0.0-SNAPSHOT"]
+            [lein-noir "1.2.1"]
+            [com.stuartsierra/lazytest "1.2.3"]
+            [org.cloudhoist/pallet-lein "0.5.0"]
+            [pallet/lein-template "0.2.3"]]
+  :search-page-size "30"
+  :repositories {"stuart" "http://stuartsierra.com/maven2"}}}
+
