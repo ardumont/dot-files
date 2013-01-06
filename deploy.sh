e106d251da9fe469c40d184e9049ba1f960f0bb9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-01-06 12:25:22 +0100

    Refactoring the deploying script

--- deploy.sh
+++ deploy.sh
@@ -1,61 +1,44 @@
-#!/bin/bash
+#!/bin/bash -xe
 
-# This script aims at deploying the links to your home
+# This script deploys the links to your home:
 # Use: /path/to/the/script/$0
 # Example: ~/repo/perso/dot-files/deploy.sh
 # The use of the absolute name for calling the script is unavoidable
 
-REPO_WORK=$SRC/work/bin
+## Functions
+
+create-links-from-list() {
+    for i in $*; do
+        # destroy any existing file
+        rm -f $HOME/$i
+        # then create the link
+        ln -s $REPO_DOTFILES/$i $HOME/
+    done
+}
+
+## Main
+
 REPO_DOTFILES=$(dirname $0)
+REPO_WORK=~/work/
 
-rm $HOME/.stumpwmrc
-ln -s $REPO_DOTFILES/.stumpwmrc $HOME/
+# standard files
+FILES=".stumpwmrc .profile .bashrc .bashrc-env .bashrc-path .bash_aliases .bashrc-prompt .tmux.conf .ratpoisonrc .xmodmaprc .vimrc .keysnail.js .git-completion.bash .conkerorrc"
 
-# $HOME/.bash*
-rm $HOME/.profile
-ln -s $REPO_DOTFILES/.profile $HOME/
+# some more specific work files
+FILES_WORK=".bashrc-work"
 
-rm $HOME/.bashrc
-ln -s $REPO_DOTFILES/.bashrc $HOME/
+# Simple dot-files
 
-rm $HOME/.bashrc-env
-ln -s $REPO_DOTFILES/.bashrc-env $HOME/
+create-links-from-list $FILES
 
-rm $HOME/.bashrc-path
-ln -s $REPO_DOTFILES/.bashrc-path $HOME/
+# possible work dot-files
 
-rm $HOME/.bash_aliases
-ln -s $REPO_DOTFILES/.bash_aliases $HOME/
-
-rm $HOME/.bashrc-prompt
-ln -s $REPO_DOTFILES/.bashrc-prompt $HOME/
-
-rm $HOME/.tmux.conf
-ln -s $REPO_DOTFILES/.tmux.conf $HOME/
-
-rm $HOME/.ratpoisonrc
-ln -s $REPO_DOTFILES/.ratpoisonrc $HOME/
-
-rm $HOME/.xmodmaprc
-ln -s $REPO_DOTFILES/.xmodmaprc $HOME/
-
-rm $HOME/.vimrc
-ln -s $REPO_DOTFILES/.vimrc $HOME/
-
-rm $HOME/.keysnail.js
-ln -s $REPO_DOTFILES/.keysnail.js $HOME/
-
-if [ -f $REPO_WORK/.bashrc-work ]; then
-    rm $HOME/.bashrc-work
-    ln -s $REPO_WORK/.bashrc-work $HOME/
+if [ -d $REPO_WORK ]; then
+    create-links-from-list $FILES_WORK
 fi
 
-rm $HOME/.git-completion.bash
-ln -s $REPO_DOTFILES/.git-completion.bash $HOME/
-
-rm $HOME/.conkerorrc
-ln -s $REPO_DOTFILES/.conkerorrc $HOME/
+# some stuff for clojure
 
 mkdir -p ~/.lein
-rm $HOME/.lein/profiles.clj
+rm -f $HOME/.lein/profiles.clj
 ln -s $REPO_DOTFILES/profiles.clj $HOME/.lein/profiles.clj
