01feac025cc0b80da51bf0029680731c4e22adeb
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-01-12 21:09:50 +0100

    Adding ritz

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,6 @@
 {:user
- {:plugins [[lein-swank "1.4.4"]
+ {:plugins [[lein-ritz "0.6.0"]
+            [lein-swank "1.4.4"]
             [lein-cljs "0.2.2"]
             [lein-difftest "1.3.7"]
             [lein-marginalia "0.7.1"]
@@ -9,6 +10,11 @@
             [com.stuartsierra/lazytest "1.2.3"]
             [org.cloudhoist/pallet-lein "0.5.0"]
             [pallet/lein-template "0.2.3"]]
+  :dependencies [[ritz/ritz-nrepl-middleware "0.6.0"]]
+  :repl-options {:nrepl-middleware
+                 [ritz.nrepl.middleware.javadoc/wrap-javadoc
+                  ritz.nrepl.middleware.simple-complete/wrap-simple-complete]}
+  :hooks [ritz.add-sources]
   :search-page-size "30"
   :repositories {"stuart"              "http://stuartsierra.com/maven2"
                  "googleapis"          "http://mavenrepo.google-api-java-client.googlecode.com/hg/"
