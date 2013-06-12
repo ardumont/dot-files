47931156cbcfbbf8076560d41cab5ada02be801a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-12 19:36:25 +0200

    Defining macro key as in emacs.

--- .keysnail.js
+++ .keysnail.js
@@ -13,8 +13,8 @@
 key.quitKey              = "C-g";
 key.helpKey              = "<f1>";
 key.escapeKey            = "C-q";
-key.macroStartKey        = "<f3>";
-key.macroEndKey          = "<f4>";
+key.macroStartKey        = "C-x (";
+key.macroEndKey          = "C-x )";
 key.universalArgumentKey = "C-u";
 key.negativeArgument1Key = "C--";
 key.negativeArgument2Key = "C-M--";
