f308bd0d28c0afc88655f9e74100571c9f8b5b8b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-28 09:23:49 +0200

    Upgrade clojure-complete to 0.2.3

--- profiles.clj
+++ profiles.clj
@@ -6,20 +6,20 @@
                   [lein-pprint               "1.1.1"]
                   [lein-midje                "3.0.1"]
                   [lein-noir                 "1.2.1"]
-                  [lein-localrepo            "0.5.0"]
+                  ;; [lein-localrepo            "0.5.0"]
                   [com.stuartsierra/lazytest "1.2.3"]
                   [com.palletops/pallet-lein "0.6.0-beta.7"]
-                  ;;            [pallet/lein-template      "0.2.10"]
+                  [pallet/lein-template      "0.2.10"]
                   [hiccup-bridge             "1.0.0-SNAPSHOT"]
                   [lein-cljsbuild            "0.3.0"]
                   [lein-droid                "0.1.0-beta6"]
-                  [lein-outdated             "1.0.0"]
+                  ;; [lein-outdated             "1.0.0"]
                   ]
         :pallet {:dependencies [[org.virtualbox/vboxjws "4.2.6"]]
                  :source-paths ["src"] :resource-paths []}
         :dependencies [
                        ;; [ritz/ritz-nrepl-middleware "0.7.0"]
-                       [clojure-complete           "0.2.2"]]
+                       [clojure-complete           "0.2.3"]]
         ;; :repl-options {:nrepl-middleware
         ;;                [ritz.nrepl.middleware.javadoc/wrap-javadoc
         ;;                 ritz.nrepl.middleware.simple-complete/wrap-simple-complete]}
