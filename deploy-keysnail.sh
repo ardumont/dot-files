e3a2630ef572c75854b6f7b161592b16510f9f4b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-10 13:42:48 +0100

    Add the missed folder in the user-profile/extensions/<keysnail-extension-folder>

--- deploy-keysnail.sh
+++ deploy-keysnail.sh
@@ -14,17 +14,22 @@ mkdir -p $FF_FOLDER
 
 ######### DEPLOY
 
-FOLDER_DESTINATION_RELATIVE=$(grep "Path=" $FF_FOLDER/profiles.ini | tail -1 | cut -f2 -d'=')
-FOLDER_DESTINATION=$FF_FOLDER/$FOLDER_DESTINATION_RELATIVE
-FOLDER_KEYSNAIL=$FOLDER_DESTINATION/keysnail
+FOLDER_USER_PROFILE_RELATIVE=$(grep "Path=" $FF_FOLDER/profiles.ini | tail -1 | cut -f2 -d'=')
+FOLDER_USER_PROFILE=$FF_FOLDER/$FOLDER_USER_PROFILE_RELATIVE
+FOLDER_FIREFOX_EXTENSIONS=$FOLDER_USER_PROFILE/extensions
+KEYSNAIL_EXTENSION=keysnail@mooz.github.com
+FOLDER_FIREFOX_EXTENSION_KEYSNAIL=$FOLDER_FIREFOX_EXTENSIONS/$KEYSNAIL_EXTENSION
+FOLDER_KEYSNAIL_EXTENSIONS=$FOLDER_USER_PROFILE/keysnail
 
-# Cleanup eventual old folder
-[ -d $FOLDER_KEYSNAIL -a ! -h $FOLDER_KEYSNAIL ] && rm -rf $FOLDER_KEYSNAIL
+# Cleanup eventual old installation (hard-coded file and not a symlink)
+[ -d $FOLDER_FIREFOX_EXTENSION_KEYSNAIL -a ! -h $FOLDER_FIREFOX_EXTENSION_KEYSNAIL ] && rm -rf $FOLDER_FIREFOX_EXTENSION_KEYSNAIL
+[ -d $FOLDER_KEYSNAIL_EXTENSIONS -a ! -h $FOLDER_KEYSNAIL_EXTENSIONS ] && rm -rf $FOLDER_KEYSNAIL_EXTENSIONS
 
 # Reference the keysnail extension
-cp "$FOLDER_DESTINATION/extensions.ini" "$FOLDER_DESTINATION/extensions.bak.ini"
-runhaskell $REPO_DOTFILES/hs/LoadAndUpdateIni.hs "$FOLDER_DESTINATION/extensions.bak.ini" "$FOLDER_DESTINATION/extensions/keysnail@mooz.github.com" > "$FOLDER_DESTINATION/extensions.ini"
+cp "$FOLDER_USER_PROFILE/extensions.ini" "$FOLDER_USER_PROFILE/extensions.bak.ini"
+runhaskell $REPO_DOTFILES/hs/LoadAndUpdateIni.hs "$FOLDER_USER_PROFILE/extensions.bak.ini" "$FOLDER_FIREFOX_EXTENSIONS/$KEYSNAIL_EXTENSION" > "$FOLDER_USER_PROFILE/extensions.ini"
 
 # Force link creation
-ln -nsf $REPO_DOTFILES/keysnail $FOLDER_KEYSNAIL
-ln -nsf $REPO_DOTFILES/.keysnail.js $HOME
+ln -nsf $REPO_DOTFILES/$KEYSNAIL_EXTENSION $FOLDER_FIREFOX_EXTENSIONS # install firefox extension keysnail
+ln -nsf $REPO_DOTFILES/keysnail $FOLDER_KEYSNAIL_EXTENSIONS           # install keysnail extensions
+ln -nsf $REPO_DOTFILES/.keysnail.js $HOME                             # install user's keysnail preference file
