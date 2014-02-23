3f3a799785f06e9d270b158ee7e4f6891dc8b33c
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-23 14:41:52 +0100

    Add empty .stalonetrayrc

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,7 @@ create-links-from-list() {
 REPO_DOTFILES=$(dirname $0)
 
 # standard files
-FILES=".stumpwmrc .stumpwm-functions.lisp .profile .bashrc .bashrc-env .bashrc-path .bash_aliases .bashrc-prompt .tmux.conf .ratpoisonrc .vimrc .git-completion.bash .conkerorrc .sbclrc .xmonad .Xmodmap quicklisp .ctags .gitignore_global .zshrc Makefile .gitconfig .xinitrc"
+FILES=".stumpwmrc .stumpwm-functions.lisp .profile .bashrc .bashrc-env .bashrc-path .bash_aliases .bashrc-prompt .tmux.conf .ratpoisonrc .vimrc .git-completion.bash .conkerorrc .sbclrc .xmonad .Xmodmap quicklisp .ctags .gitignore_global .zshrc Makefile .gitconfig .xinitrc .stalonetrayrc"
 
 create-links-from-list $REPO_DOTFILES $HOME $FILES
 
