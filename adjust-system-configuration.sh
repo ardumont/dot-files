c74b52f3a5f2ac148610211fd4abe2505495a39c
Antoine R. Dumont <eniotna.t@gmail.com>
2014-05-08 12:39:04 +0200

    Refactor - Remove redundancy

--- adjust-system-configuration.sh
+++ adjust-system-configuration.sh
@@ -1,16 +1,24 @@
 #!/bin/bash -x
-set +e # deactivate fail-fast
 
 # Use: $0
 # A script to adjust environment settings to match my needs
 
 #### Suspend on lid close event
 
+ACPI_SUPPORT_FILE=/etc/default/acpi-support
+# Expressions to look for/replace
+EXPRESSION="LID_SLEEP=true"
+EXPRESSION_TO_LOOK_FOR="#$EXPRESSION"
+
 # I no longer used desktop environment (so no power management, etc...) but windows manager (like stumpwm, xmonad, etc...)
-# On my actual system, the LID_SLEEP variable in /etc/default/acpi-support is commented so no suspension takes place when the laptop lid is closed
+# On my actual system, the LID_SLEEP variable in $ACPI_SUPPORT_FILE is commented so no suspension takes place when the laptop lid is closed
 # This will modify such variable if need be
 
-if [ -f /etc/default/acpi-support ]; then
-    grep -q "#LID_SLEEP=true" /etc/default/acpi-support
-    [ $? -eq 0 ] && sudo sed 's/#LID_SLEEP=true/LID_SLEEP=true/g' -i /etc/default/acpi-support
+if [ -f $ACPI_SUPPORT_FILE ]; then
+    set +e # deactivate fail-fast
+
+    grep -q "$EXPRESSION_TO_LOOK_FOR" $ACPI_SUPPORT_FILE
+    [ $? -eq 0 ] && sudo sed 's/$EXPRESSION_TO_LOOK_FOR/$EXPRESSION/g' -i $ACPI_SUPPORT_FILE
 fi
+
+exit 0
