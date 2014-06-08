53ac5831a227559213cb8752f2f39e6dac8bd8cd
Antoine R. Dumont <eniotna.t@gmail.com>
2014-06-08 21:18:08 +0200

    Refactor - Update configuration as point free style

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -54,7 +54,7 @@ myFocusedBorderColor = "#ff0000"
 -- Prefix key
 --
 prefix :: String -> String
-prefix key = "C-t " ++ key
+prefix = ("C-t " ++)
 
 zenityText :: String -> String
 zenityText s = "zenity --info --text '" ++ s ++ "'"
