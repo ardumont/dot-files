e0f33d157e2d60229abdace7d36a004d1428e9d1
Antoine R. Dumont <eniotna.t@gmail.com>
2014-07-20 14:24:15 +0200

    NixOS - Refactor - remove non working cmd

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -106,7 +106,7 @@
 
   programs.ssh.startAgent = false; # do not start agent (gpg-agent will be started)
 
-  powerManagement.resumeCommands = "xscreensaver-command -display 0:0 -lock"; #Commands executed after the system resumes from suspend-to-RAM.
+  powerManagement.resumeCommands = "xscreensaver-command -lock"; #Commands executed after the system resumes from suspend-to-RAM.
 
   # List services that you want to enable:
   services = {
