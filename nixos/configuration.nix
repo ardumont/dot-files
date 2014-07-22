d0f90a7e2c7e594dea400470614ee41b8dc7b29f
Antoine R. Dumont <eniotna.t@gmail.com>
2014-07-22 21:07:24 +0200

    NixOS - Refactor - Use the same formatting everywhere

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -124,11 +124,10 @@
   services = {
     acpid = {
       enable = true;     # acpi
-      lidEventCommands = # suspend on lid close
-        ''
+      lidEventCommands = ''
         # suspend on lid close event or do nothing
         grep -q open /proc/acpi/button/lid/LID0/state && exit 0 || systemctl suspend
-        '';
+        ''; # suspend on lid close
       };
 
     openssh.enable = true;  # OpenSSH daemon.
