32ead2f839e34639efbd8c9f742c13fe9122a6cd
Antoine R. Dumont <eniotna.t@gmail.com>
2014-10-24 12:39:40 +0200

    Nix - Add a main default dev environment from which other will derive

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -2,61 +2,75 @@
 
 {
   # source: https://nixos.org/wiki/Howto_develop_software_on_nixos
-  packageOverrides = self : with pkgs; rec {
-    # install: nix-env -i env-sdl
-    # load: load-env-sdl
-    sdlEnv = self.myEnvFun {
-        name = "sdl";
-        buildInputs = [ stdenv SDL SDL_image SDL_ttf SDL_gfx cmake SDL_net pkgconfig ];
+  packageOverrides = self : with pkgs; with sourceAndTags;
+    # default dev environment with common needed tools
+    let defaultDevEnv = { name, buildInputs ? [], cTags ? [], extraCmds ? ""}:
+      pkgs.myEnvFun {
+        inherit name;
+        buildInputs = buildInputs
+          ++ map (x : sourceWithTagsDerivation ( (addCTaggingInfo x ).passthru.sourceWithTags ) ) cTags
+          ++ [ gitFull zsh keychain ];
+        extraCmds = ''
+          ${extraCmds}
+          HOME=${builtins.getEnv "HOME"}
+          . ~/.zshrc
+        '';
     };
+    in rec {
 
-    # default haskell environment to provide when dev
-    # install: nix-env -iA nixos.pkgs.haskell
-    #          nix-env -i env-haskell
-    # load: load-env-haskell
-    hsEnv = self.myEnvFun {
-      name = "haskell";
-      buildInputs = [
-        haskellPackages.cabalInstall
-        haskellPackages.cabal2nix
-        haskellPackages.lens
-        haskellPackages.hlint
-        haskellPackages.hdevtools
-        haskellPackages.zlib
-        haskellPackages.mtl
-        haskellPackages.HUnit
-        haskellPackages.QuickCheck
-        # haskellPackages.Agda
-      ];
-    };
+      # install: nix-env -i env-sdl
+      # load: load-env-sdl
+      sdlEnv = self.myEnvFun {
+          name = "sdl";
+          buildInputs = [ stdenv SDL SDL_image SDL_ttf SDL_gfx cmake SDL_net pkgconfig ];
+      };
 
-    # default purescript environment
-    # install: nix-env -i env-purescript
-    # load: load-env-purescript
-    purescriptEnv = self.myEnvFun {
-      name = "purescript";
-      buildInputs = [
-        haskellPackages.purescript
-        nodejs
-        nodePackages.npm
-        nodePackages.jshint
-        nodePackages.grunt-cli
-        nodePackages.bower
-      ];
-    };
+      # default haskell environment to provide when dev
+      # install: nix-env -iA nixos.pkgs.haskell
+      #          nix-env -i env-haskell
+      # load: load-env-haskell
+      hsEnv = defaultDevEnv {
+        name = "haskell";
+        buildInputs = [
+          haskellPackages.cabalInstall
+          haskellPackages.cabal2nix
+          haskellPackages.lens
+          haskellPackages.hlint
+          haskellPackages.hdevtools
+          haskellPackages.zlib
+          haskellPackages.mtl
+          haskellPackages.HUnit
+          haskellPackages.QuickCheck
+          # haskellPackages.Agda
+        ];
+      };
 
-    # default xmonad env
-    # install: nix-env -i env-xmonad
-    # load: load-env-xmonad
-    xmonadEnv = self.myEnvFun {
-      name = "xmonad";
-      buildInputs = with pkgs.haskellPackages; [
-       xmonad
-       xmonadContrib
-       xmonadExtras
-       xmobar
-      ];
-    };
+      # default purescript environment
+      # install: nix-env -i env-purescript
+      # load: load-env-purescript
+      purescriptEnv = defaultDevEnv {
+        name = "purescript";
+        buildInputs = [
+          haskellPackages.purescript
+          nodejs
+          nodePackages.npm
+          nodePackages.jshint
+          nodePackages.grunt-cli
+          nodePackages.bower
+        ];
+      };
 
-  };
+      # default xmonad env
+      # install: nix-env -i env-xmonad
+      # load: load-env-xmonad
+      xmonadEnv = defaultDevEnv {
+        name = "xmonad";
+        buildInputs = with pkgs.haskellPackages; [
+         xmonad
+         xmonadContrib
+         xmonadExtras
+         xmobar
+        ];
+      };
+   };
 }
