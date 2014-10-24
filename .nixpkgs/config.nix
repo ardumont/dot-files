0f86b52df84b42cdadd627a9e1f5dd1170e19841
Antoine R. Dumont <eniotna.t@gmail.com>
2014-10-24 10:35:43 +0200

    Nix - Rework development environment definition + command to install/load them

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,37 +1,45 @@
 { pkgs }:
 
 {
-   packageOverrides = self : with pkgs; rec {
-     # dummy env to understand nix-env - https://nixos.org/wiki/Howto_develop_software_on_nixos
-     someEnvNameEnv = self.myEnvFun {
-         name = "someEnvName";
-         buildInputs = [ stdenv pkgconfig ];
-     };
+  packageOverrides = self : with pkgs; rec {
+    # dummy env to understand nix-env - https://nixos.org/wiki/Howto_develop_software_on_nixos
+    someEnvNameEnv = self.myEnvFun {
+        name = "someEnvName";
+        buildInputs = [ stdenv pkgconfig ];
+    };
 
-     sdlEnv = self.myEnvFun {
-         name = "sdl";
-         buildInputs = [ stdenv SDL SDL_image SDL_ttf SDL_gfx cmake SDL_net pkgconfig ];
-     };
+    sdlEnv = self.myEnvFun {
+        name = "sdl";
+        buildInputs = [ stdenv SDL SDL_image SDL_ttf SDL_gfx cmake SDL_net pkgconfig ];
+    };
 
-     # default haskell environment to provide when dev (install: nix-env -iA nixos.pkgs.hsEnv)
-     hsEnv = self.haskellPackages.ghcWithPackages (pack : [
-         pack.cabalInstall
-         pack.cabal2nix
-         pack.lens
-         pack.hlint
-         pack.hdevtools
-         pack.zlib
-         pack.mtl
-         pack.HUnit
-         pack.QuickCheck
-         # pack.Agda
-      ]);
+    # default haskell environment to provide when dev
+    # install: nix-env -iA nixos.pkgs.hsEnv
+    # load: load-hsEnv
+    hsEnv = self.myEnvFun {
+      name = "haskell";
+      buildInputs = [
+        haskellPackages.cabalInstall
+        haskellPackages.cabal2nix
+        haskellPackages.lens
+        haskellPackages.hlint
+        haskellPackages.hdevtools
+        haskellPackages.zlib
+        haskellPackages.mtl
+        haskellPackages.HUnit
+        haskellPackages.QuickCheck
+        # haskellPackages.Agda
+      ];
+    };
 
+    # default purescript environment
+    # install: nix-env -i env-purescript
+    # load: load-env-purescript
     purescriptEnv = self.myEnvFun {
       name = "purescript";
       buildInputs = [
         haskellPackages.purescript
-        nodePackages.nodejs
+        nodejs
         nodePackages.npm
         nodePackages.jshint
         nodePackages.grunt-cli
@@ -39,12 +47,18 @@
       ];
     };
 
-    xmonadHsEnv = self.haskellPackages.ghcWithPackages (pack : [
-       pack.xmonad
-       pack.xmonadContrib
-       pack.xmonadExtras
-       pack.xmobar
-     ]);
+    # default xmonad env
+    # install: nix-env -i env-xmonad
+    # load: load-env-xmonad
+    xmonadEnv = self.myEnvFun {
+      name = "xmonad";
+      buildInputs = [
+       haskellPackages.xmonad
+       haskellPackages.xmonadContrib
+       haskellPackages.xmonadExtras
+       haskellPackages.xmobar
+      ];
+    };
 
-   };
- }
+  };
+}
