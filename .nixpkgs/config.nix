f570f20d43c0245dc8a1e4c0ae4751a1305badb5
Antoine R. Dumont <eniotna.t@gmail.com>
2014-10-24 12:59:22 +0200

    Nix - Use zsh as the default shell in dev env

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -7,14 +7,15 @@
     let defaultDevEnv = { name, buildInputs ? [], cTags ? [], extraCmds ? ""}:
       pkgs.myEnvFun {
         inherit name;
+        shell = "/var/run/current-system/sw/bin/zsh";
         buildInputs = buildInputs
           ++ map (x : sourceWithTagsDerivation ( (addCTaggingInfo x ).passthru.sourceWithTags ) ) cTags
           ++ [ gitFull zsh keychain ];
         extraCmds = ''
           HOME=${builtins.getEnv "HOME"}
-          # . ~/.zshrc
           ${extraCmds}
         '';
+
     };
     in rec {
 
