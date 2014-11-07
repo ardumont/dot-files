db60f6874699eac0b4e999e24c3ba3442e819ebc
Antoine R. Dumont <eniotna.t@gmail.com>
2014-11-07 12:57:09 +0100

    Split nixos configuration into perimeter files (to ease maintenance)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -7,6 +7,9 @@
 {
   imports = [
       ./hardware-configuration.nix # Include the results of the hardware scan.
+      ./network.nix
+      ./users.nix
+      ./packages.nix
     ];
 
   boot = {
@@ -25,25 +28,6 @@
     kernel.sysctl."fs.inotify.max_user_watches" = 100000;
   };
 
-  networking = {
-    hostName = "dagobah";    # Define your hostname.
-
-    # wireless = {
-    #   enable = true;                # Enables wireless
-    #   userControlled.enable = true; # user can play with wifi
-    #   interfaces = [ "wlp1s0" ];    # explicit the interfaces the user can modify
-    # };
-
-    networkmanager.enable = true; # nm, nmcli, etc... incompatible with networking.wireless
-    extraHosts = ''
-      192.168.0.10 dagobah
-      192.168.0.11 chris-host
-      192.168.0.13 job
-      192.168.0.14 myrkr
-      192.168.0.20 nas
-    '';
-  };
-
   time.timeZone = "Europe/Paris";
 
   # Select internationalisation properties.
@@ -53,106 +37,6 @@
     defaultLocale = "en_US.UTF-8";
     supportedLocales = [ "en_US.UTF-8/UTF-8" ];
   };
-
-  nixpkgs.config = {
-    allowUnfree = true;
-
-    firefox = {
-      enableAdobeFlash = true;
-      enableGoogleTalkPlugin = true;
-    };
-
-    chromium = {
-      enableGoogleTalkPlugin = true;
-      enablePepperFlash = true; # Chromium's non-NSAPI alternative to Adobe Flash
-      enablePepperPDF = true;
-    };
-  };
-
-  # List packages installed in system profile. To search by name, run:
-  # nix-env -qaP | grep wget
-  environment.systemPackages = with pkgs; [
-    nix-repl
-    gnome3.nautilus gnome3.gnome_settings_daemon
-    gnome3.eog pinta scrot
-    gnome3.totem vlc mplayer2 x264
-    transmission_gtk
-    audacious
-    linuxPackages.virtualbox
-    evince fbreader
-    filezilla
-    gnome.zenity
-    git gitAndTools.tig gitAndTools.hub gitg
-    gnupg gnupg1 pinentry
-    pmount file
-    wget curl tree
-    gcc gnumake
-    dropbox dropbox-cli
-    trayer
-    networkmanagerapplet
-    x11 xlibs.xmessage xlibs.xmodmap xdotool x11_ssh_askpass xscreensaver xlibs.xbacklight xlibs.xdpyinfo xlibs.xkill
-    mosh
-    offlineimap mu
-    most
-    xclip pass keychain
-    htop powertop
-    emacs texinfo w3m
-    tmux rxvt_unicode bind
-    tcsh bash zsh python ruby
-    zlib
-    firefoxWrapper chromium conkeror
-    graphviz
-    nmap netcat wireshark
-    p7zip unrar unzip
-    acpi acpid acpitool
-    mplayer vlc
-    clojure leiningen jdk
-    gparted
-    binutils
-    pmutils
-    autojump
-    inotifyTools
-    unetbootin
-    alsaUtils
-    lsof
-    vagrant
-    darcs
-#    rubyLibs.bundler
-    haskellPackages.pandoc
-    haskellPlatform
-    haskellPlatform.ghc
-    haskellPackages.ncurses
-    (haskellPackages.ghcWithPackages (self : [
-       self.cabalInstall
-       self.xmonad
-       self.xmonadContrib
-       self.xmonadExtras
-       self.xmobar
-       self.cabal2nix
-       self.lens
-       self.hlint
-       self.hdevtools
-       self.zlib
-       self.mtl
-       self.HUnit
-       self.QuickCheck
-       self.hoogle
-       # self.Agda
-    ]))
-    rlwrap
-    fortune cowsay
-    ffmpeg
-    simplescreenrecorder
-    keymon
-    imagemagick
-    libreoffice
-    docker
-    cups samba
-    telnet
-    transmission_gtk
-    nodejs rhino nodePackages.npm nodePackages.jshint nodePackages.grunt-cli nodePackages.npm2nix nodePackages.bower2nix
-    ncurses
-  ];
 
   programs.ssh.startAgent = false; # do not start agent (gpg-agent will be started)
 
@@ -245,24 +129,6 @@
        # pkgs.vistafonts
        # pkgs.wqy_zenhei
     ];
-  };
-
-  nix.trustedBinaryCaches = [ "http://hydra.nixos.org" "http://cache.nixos.org" ];
-
-  users = {
-    defaultUserShell = "/var/run/current-system/sw/bin/zsh";
-
-    # Define a user account. Don't forget to set a password with ‘passwd’.
-    extraUsers = [{
-      description = "Antoine R. Dumont";
-      name = "tony";
-      group = "users";
-      uid = 1000;
-      createHome = true;
-      home = "/home/tony";
-      extraGroups = [ "users" "wheel" "audio" "video" "vboxusers" "docker" ];
-      useDefaultShell = true;
-    }];
   };
 
   security = {
---
+++ nixos/packages.nix
@@ -0,0 +1,105 @@
+{ config, pkgs, ... }:
+
+{
+  nix.trustedBinaryCaches = [ "http://hydra.nixos.org" "http://cache.nixos.org" ];
+
+  nixpkgs.config = {
+    allowUnfree = true;
+
+    firefox = {
+      enableAdobeFlash = true;
+      enableGoogleTalkPlugin = true;
+    };
+
+    chromium = {
+      enableGoogleTalkPlugin = true;
+      enablePepperFlash = true; # Chromium's non-NSAPI alternative to Adobe Flash
+      enablePepperPDF = true;
+    };
+  };
+
+  # List packages installed in system profile. To search by name, run:
+  # nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    nix-repl
+    gnome3.nautilus gnome3.gnome_settings_daemon
+    gnome3.eog pinta scrot
+    gnome3.totem vlc mplayer2 x264
+    transmission_gtk
+    audacious
+    linuxPackages.virtualbox
+    evince fbreader
+    filezilla
+    gnome.zenity
+    git gitAndTools.tig gitAndTools.hub gitg
+    gnupg gnupg1 pinentry
+    pmount file
+    wget curl tree
+    gcc gnumake
+    dropbox dropbox-cli
+    trayer
+    networkmanagerapplet
+    x11 xlibs.xmessage xlibs.xmodmap xdotool x11_ssh_askpass xscreensaver xlibs.xbacklight xlibs.xdpyinfo xlibs.xkill
+    mosh
+    offlineimap mu
+    most
+    xclip pass keychain
+    htop powertop
+    emacs texinfo w3m
+    tmux rxvt_unicode bind
+    tcsh bash zsh python ruby
+    zlib
+    firefoxWrapper chromium conkeror
+    graphviz
+    nmap netcat wireshark
+    p7zip unrar unzip
+    acpi acpid acpitool
+    mplayer vlc
+    clojure leiningen jdk
+    gparted
+    binutils
+    pmutils
+    autojump
+    inotifyTools
+    unetbootin
+    alsaUtils
+    lsof
+    vagrant
+    darcs
+#    rubyLibs.bundler
+    haskellPackages.pandoc
+    haskellPlatform
+    haskellPlatform.ghc
+    haskellPackages.ncurses
+    (haskellPackages.ghcWithPackages (self : [
+       self.cabalInstall
+       self.xmonad
+       self.xmonadContrib
+       self.xmonadExtras
+       self.xmobar
+       self.cabal2nix
+       self.lens
+       self.hlint
+       self.hdevtools
+       self.zlib
+       self.mtl
+       self.HUnit
+       self.QuickCheck
+       self.hoogle
+       # self.Agda
+    ]))
+    rlwrap
+    fortune cowsay
+    ffmpeg
+    simplescreenrecorder
+    keymon
+    imagemagick
+    libreoffice
+    docker
+    cups samba
+    telnet
+    transmission_gtk
+    nodejs rhino nodePackages.npm nodePackages.jshint nodePackages.grunt-cli nodePackages.npm2nix nodePackages.bower2nix
+    ncurses
+  ];
+}
