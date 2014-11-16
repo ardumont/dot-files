2c5c30fafdecfdefad2922a39e23452ddcfd76ec
Antoine R. Dumont <eniotna.t@gmail.com>
2014-11-16 11:16:40 +0100

    NixOS - Add lenovo configuration (really need now to factor this)

---
+++ nixos/lenovo/network.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+  # nix.proxy = "http://login:mdp@proxy:port";
+  networking = {
+    hostName = "lenovo";    # Define your hostname.
+
+    # wireless = {
+    #   enable = true;                # Enables wireless
+    #   userControlled.enable = true; # user can play with wifi
+    #   interfaces = [ "wlp1s0" ];    # explicit the interfaces the user can modify
+    # };
+
+    networkmanager.enable = true; # nm, nmcli, etc... incompatible with networking.wireless
+    extraHosts = ''
+      192.168.0.10 dagobah
+      192.168.0.11 chris-host
+      192.168.0.13 job
+      192.168.0.14 myrkr
+      192.168.0.15 lenovo
+      192.168.0.20 nas
+    '';
+  };
+
+}
---
+++ nixos/lenovo/packages.nix
@@ -0,0 +1,108 @@
+{ config, pkgs, ... }:
+
+{
+  nix.trustedBinaryCaches = [ "http://hydra.nixos.org" "http://cache.nixos.org" ];
+
+  nixpkgs.config = {
+    allowUnfree = true;
+
+    firefox = {
+      enableAdobeFlash = true;
+      enableGoogleTalkPlugin = true;
+    };
+
+    chromium = {
+      enableGoogleTalkPlugin = true;
+      enablePepperFlash = true; # Chromium's non-NSAPI alternative to Adobe Flash
+      enablePepperPDF = true;
+    };
+  };
+
+  # List packages installed in system profile. To search by name, run:
+  # nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    peco
+    nix-repl
+    gnome3_12.nautilus gnome3_12.gnome_settings_daemon
+    gnome3_12.eog pinta scrot
+    gnome3_12.totem vlc mplayer2 x264
+    gnome3_12.zenity
+#    transmission_gtk
+#    audacious
+    linuxPackages.virtualbox packer
+    evince fbreader
+    filezilla
+    git gitAndTools.tig gitAndTools.hub gitg meld
+    mercurialFull
+    gnupg gnupg1 pinentry
+    pmount file
+    wget curl tree
+    gcc gnumake
+    dropbox dropbox-cli
+    trayer
+    networkmanagerapplet
+    x11 xlibs.xmessage xlibs.xmodmap xdotool x11_ssh_askpass xscreensaver xlibs.xbacklight xlibs.xdpyinfo xlibs.xkill
+    mosh
+    offlineimap mu
+    most
+    xclip xsel pass keychain
+    htop powertop
+    emacs texinfo w3m
+    tmux bind rxvt_unicode urxvt_perls
+    bash zsh ruby
+    python3 python34Packages.pip
+    zlib
+    firefoxWrapper chromium conkeror
+    graphviz
+    nmap netcat wireshark
+    p7zip unrar unzip
+    acpi acpid acpitool
+    mplayer vlc
+    clojure leiningen jdk
+    gparted
+    binutils
+    pmutils
+    autojump
+    inotifyTools
+    #unetbootin
+    alsaUtils
+    lsof
+    vagrant
+    darcs
+#    rubyLibs.bundler
+    haskellPackages.pandoc
+#    haskellPlatform
+#    haskellPlatform.ghc
+    haskellPackages.ncurses
+    (haskellPackages.ghcWithPackages (self : [
+       self.cabalInstall
+       self.xmonad
+       self.xmonadContrib
+       self.xmonadExtras
+       self.xmobar
+       self.cabal2nix
+       self.lens
+       self.hlint
+       self.hdevtools
+       self.zlib
+       self.mtl
+       self.HUnit
+       self.QuickCheck
+       self.hoogle
+       # self.Agda
+    ]))
+    rlwrap
+    fortune cowsay
+    ffmpeg
+    simplescreenrecorder
+    keymon
+    imagemagick
+    libreoffice
+    docker
+    cups samba
+    telnet
+    # rhino
+    nodejs nodePackages.npm nodePackages.jshint nodePackages.grunt-cli nodePackages.npm2nix nodePackages.bower2nix
+    ncurses
+  ];
+}
