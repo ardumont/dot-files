1510fa54610286e951f012b8e35842ecfbf4577f
Antoine R. Dumont <eniotna.t@gmail.com>
2014-11-17 10:25:32 +0100

    Layout - Fix right screen's starting position

--- .layouts/lenovo.sh
+++ .layouts/lenovo.sh
@@ -4,11 +4,11 @@
 
 xrandr --output LVDS1 \
        --mode 1366x768 \
-       --pos 1366x0 \
+       --pos 0x0 \
        --rotate normal \
        --output VGA1 \
        --mode 1280x1024 \
-       --pos 0x0 \
+       --pos 1366x0 \
        --rotate normal \
        --output DP1 \
        --off \
