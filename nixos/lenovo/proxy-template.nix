64f1e19befaa2e23512a75665a8f4e52575c3c6d
Antoine R. Dumont <eniotna.t@gmail.com>
2014-11-18 09:50:23 +0100

    NixOS - Add no_proxy environment variable in proxy-template.nix

--- nixos/lenovo/proxy-template.nix
+++ nixos/lenovo/proxy-template.nix
@@ -3,5 +3,8 @@
 {
   # copy this template and name it proxy.nix,
   # adapt and uncomment this according to your need for proxy
-# nix.proxy = "http://user:pass@host:port";
+  # nix.proxy = "http://login:mdp@proxy:port";
+  # environment.variables = {
+  #   no_proxy = "127.0.0.1,localhost";
+  # };
 }
