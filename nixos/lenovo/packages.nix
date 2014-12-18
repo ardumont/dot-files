985bb5fd5aa2de2727bb0bffdf72456e91ee96eb
Antoine R. Dumont <eniotna.t@gmail.com>
2014-12-18 11:29:45 +0100

    NixOS - Add switch deps

--- nixos/lenovo/packages.nix
+++ nixos/lenovo/packages.nix
@@ -28,6 +28,7 @@
   # List packages installed in system profile. To search by name, run:
   # nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
+    which
     peco
     nix-repl
     pidgin-with-plugins
