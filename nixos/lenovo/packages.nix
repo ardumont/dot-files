6fea5e43d489d79c85c5acd31013d21b7194ed27
Antoine R. Dumont <eniotna.t@gmail.com>
2014-12-18 14:29:46 +0100

    NixOS - Add misc utility tools (fuser, ...)

--- nixos/lenovo/packages.nix
+++ nixos/lenovo/packages.nix
@@ -28,7 +28,7 @@
   # List packages installed in system profile. To search by name, run:
   # nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    which
+    which psmisc
     peco
     nix-repl
     pidgin-with-plugins
