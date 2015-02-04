a84f2467fd9a286307cef63bd9d05c788eeaf340
Antoine R. Dumont <eniotna.t@gmail.com>
2015-02-04 16:14:00 +0100

    NixOS - lenovo - Remove extra config

--- nixos/lenovo/ssh.nix
+++ nixos/lenovo/ssh.nix
@@ -3,12 +3,5 @@
 {
   programs.ssh = {
     startAgent = false; # do not start agent (gpg-agent will be started)
-    extraConfig = ''
-      Host *
-      SendEnv LANG LC_*
-      HashKnownHosts yes
-      GSSAPIAuthentication yes
-      GSSAPIDelegateCredentials no
-    '';
   };
 }
