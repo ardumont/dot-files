df576826c88a7038de20f5001a0032c16f40666e
Antoine R. Dumont <eniotna.t@gmail.com>
2015-02-18 15:36:46 +0100

    NixOS - Lenovo - Add traceroute

--- nixos/lenovo/packages.nix
+++ nixos/lenovo/packages.nix
@@ -41,6 +41,7 @@
   # List packages installed in system profile. To search by name, run:
   # nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
+    traceroute
     p7zip
     openssh
     which psmisc
