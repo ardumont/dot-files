b705edfd0bd5cae478532131a8585efd61b01f16
Antoine R. Dumont <eniotna.t@gmail.com>
2015-03-09 09:49:31 +0100

    NixOS - Fix typo on the noProxy option

--- nixos/lenovo/proxy-template.nix
+++ nixos/lenovo/proxy-template.nix
@@ -5,6 +5,6 @@
   # adapt and uncomment this according to your need for proxy
   # nix.proxy = "http://login:mdp@proxy:port";
   # environment.variables = {
-  #   no_proxy = "127.0.0.1,localhost";
+  #   noProxy = "127.0.0.1,localhost";
   # };
 }
