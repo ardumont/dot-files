9a258607ba0dd8672d34ffaa4dc3439955341b67
Antoine R. Dumont <eniotna.t@gmail.com>
2015-03-18 23:19:49 +0100

    dot-files - Add .zshenv for ssh connection

---
+++ .zshenv
@@ -0,0 +1,2 @@
+# nix env (manual install)
+[ -f ~/.nix-profile/etc/profile.d/nix.sh ] && . ~/.nix-profile/etc/profile.d/nix.sh
--- deploy.sh
+++ deploy.sh
@@ -33,7 +33,7 @@ create-links-from-list() {
 REPO_DOTFILES=$(dirname $0)
 
 # standard files (in home directory)
-FILES=".xmonad.start .stumpwm.start .stumpwmrc .stumpwm-functions.lisp .profile .bashrc .shrc .shrc-env .shrc-path .shrc-aliases .shrc-prompt .tmux.conf .ratpoisonrc .vimrc .git-completion.bash .conkerorrc .sbclrc .xmonad .Xmodmap quicklisp .ctags .gitignore_global .zshrc Makefile .gitconfig .xinitrc .xsession .stalonetrayrc .FBReader .xscreensaver .gimpgimp-layout-1920x1080 .gimpgimp-rules .offlineimaprc .signature .signature2 .offlineimap-helpers.py .xmobarrc .Xresources .urxvt .layouts .nixpkgs .xinit .ghc"
+FILES=".xmonad.start .stumpwm.start .stumpwmrc .stumpwm-functions.lisp .profile .bashrc .shrc .shrc-env .shrc-path .shrc-aliases .shrc-prompt .tmux.conf .ratpoisonrc .vimrc .git-completion.bash .conkerorrc .sbclrc .xmonad .Xmodmap quicklisp .ctags .gitignore_global .zshrc Makefile .gitconfig .xinitrc .xsession .stalonetrayrc .FBReader .xscreensaver .gimpgimp-layout-1920x1080 .gimpgimp-rules .offlineimaprc .signature .signature2 .offlineimap-helpers.py .xmobarrc .Xresources .urxvt .layouts .nixpkgs .xinit .ghc .zshenv"
 
 create-links-from-list $REPO_DOTFILES $HOME $FILES
 
