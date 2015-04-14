2bb64667b5937875fb010895fc4656ffbd3b13e1
Antoine R. Dumont <eniotna.t@gmail.com>
2015-04-14 14:52:58 +0200

    NixOS - dagobah - format

--- nixos/dagobah/xserver.nix
+++ nixos/dagobah/xserver.nix
@@ -17,19 +17,19 @@
       twoFingerScroll = true;
 
       # - Activate palm detection
-     palmDetect = false;
+      palmDetect = false;
       # - Activate tap to click
-     tapButtons = true;
+      tapButtons = true;
       # - Activate 2 fingers tapping as right click
       # - Activate 3 fingers tapping as middle click
       buttonsMap = [ 1 3 2 ];
 
-     # additionalOptions = ''
-     #   Option "PalmDetect" "0"
-     #   Option "TapButton1" "1"
-     #   Option "TapButton2" "3"
-     #   Option "TapButton3" "2"
-     # '';
+      # additionalOptions = ''
+      #   Option "PalmDetect" "0"
+      #   Option "TapButton1" "1"
+      #   Option "TapButton2" "3"
+      #   Option "TapButton3" "2"
+      # '';
     };
 
     desktopManager = {
