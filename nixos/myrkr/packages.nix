c0bb0695b8e4557b8a9909e0e4d7420d417668b0
Antoine R. Dumont <eniotna.t@gmail.com>
2015-05-20 09:50:37 +0200

    NixOS - myrkr - add pciutils to ease hardware listing (lspci...)

--- nixos/myrkr/packages.nix
+++ nixos/myrkr/packages.nix
@@ -39,6 +39,7 @@
   # nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     html2text
+    pciutils # lspci, etc...
     nix-prefetch-scripts nix-repl nixops nox
     psmisc
     python27Packages.screenkey
