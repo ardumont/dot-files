f04fe0a68b57411ccf9f65be739938b46571a59a
Antoine R. Dumont <eniotna.t@gmail.com>
2015-05-27 16:03:59 +0200

    NixOS - Remove redundant configuration

--- nixos/corellia/services.nix
+++ nixos/corellia/services.nix
@@ -3,14 +3,6 @@
 {
   # List services that you want to enable:
   services = {
-    acpid = {
-      enable = true;     # acpi
-      lidEventCommands = ''
-        # suspend on lid close event or do nothing
-        grep -q open /proc/acpi/button/lid/LID0/state && exit 0 || systemctl suspend
-        ''; # suspend on lid close
-      };
-
     locate = {
       enable = true;
       period = "00 19 * * *"; # update db at 19h every day
