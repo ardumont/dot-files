fa5a5b71dcf32891855a85bd01d3ef7cbef3dd1e
Antoine R. Dumont <eniotna.t@gmail.com>
2015-06-07 12:41:27 +0200

    nixpkgs - Create a systemTools env
    Which in non-nixos environment, could be loaded in ~/.(bash|z)shrc

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -2,6 +2,17 @@
   emacsPackagesNg ? (import <nixpkgs> {}).emacsPackagesNg }:
 
 {
+  firefox = {
+    enableAdobeFlash = true;
+    enableGoogleTalkPlugin = true;
+  };
+
+  chromium = {
+    enableGoogleTalkPlugin = true;
+    enablePepperFlash = true; # Chromium's non-NSAPI alternative to Adobe Flash
+    enablePepperPDF = true;
+  };
+
   # Allow broken content (when using nixpkgs as git)
   allowBroken = true;
 
@@ -38,9 +49,13 @@
 
     in rec {
 
-      # xmonad-with-packages = callPackage /home/tony/repo/perso/nixpkgs/pkgs/applications/window-managers/xmonad/wrapper.nix {
-      #   ghcWithPackages = haskellngPackages.ghcWithPackages;
-      #   packages = self: [ haskellPackages.xmonadContrib haskellPackages.xmonadExtras ];
+      # override the default pidgin with plugins (empty by default)
+      pidgin-plugins = pidgin-with-plugins.override {
+        plugins = [ pidginotr skype4pidgin ];
+      };
+
+      # xmonad-with-packages = pkgs.xmonad-with-packages.override {
+      #   packages = with pkgs.haskellngPackages.ghcWithPackages; [ xmonad-contrib xmonad-extras ];
       # };
 
       # install: nix-env -i env-sdl
@@ -286,6 +301,113 @@
           python34Packages.sqlalchemy9
           python34Packages.psycopg2
           python34Packages.requests
+        ];
+      };
+
+      systemToolsEnv = defaultDevEnv {
+        name = "system";
+        buildInputs = with pkgs; [
+          bc
+          pgadmin
+          html2text
+          pciutils # lspci, etc...
+          nix-prefetch-scripts nix-repl nixops nox
+          psmisc # fuser
+          python python27Packages.screenkey
+          python3 python34Packages.pip python34Packages.flake8 python34Packages.pygit2
+          texLiveFull
+          mysql mysqlWorkbench
+          pidgin-plugins
+          skype
+          openvpn networkmanager_openvpn
+          androidsdk_4_4
+          wmname
+          feh
+          # steam steamChrootEnv # sudo init-steam-chrootenv mount-steam-chrootenv load-steam-chrootenv
+          xsane
+          jq
+          which
+          peco
+          nix-repl
+          gnome3.nautilus gnome3.gnome_settings_daemon
+          gnome3.eog pinta scrot
+          vlc x264 mplayer
+          gnome3.zenity
+          transmission_gtk
+          audacious
+          linuxPackages.virtualbox packer vagrant docker
+          evince fbreader mcomix
+          filezilla
+          git gitAndTools.tig gitAndTools.hub gitg meld libgit2
+          gnupg gnupg1 pinentry
+          pmount file
+          wget curl tree
+          gcc gnumake
+          dropbox dropbox-cli
+          trayer
+          networkmanagerapplet
+          x11 xlibs.xmessage xlibs.xmodmap xdotool x11_ssh_askpass xscreensaver xlibs.xbacklight xlibs.xdpyinfo xlibs.xkill xlibs.xhost
+          libxml2
+          mosh
+          offlineimap mu
+          most
+          xclip xsel pass pwgen keychain
+          htop # powertop
+          emacs texinfo w3m emacs24Packages.cask aspell aspellDicts.en aspellDicts.fr
+          emacs24PackagesNg.structured-haskell-mode
+          tmux bind rxvt_unicode urxvt_perls
+          bash zsh ruby
+          bundler
+          zlib
+          firefoxWrapper conkeror chromium
+          graphviz
+          nmap netcat wireshark
+          p7zip unrar zip unzip
+          acpi acpid acpitool
+          clojure leiningen jdk
+          gparted testdisk
+          binutils
+          pmutils
+          autojump
+          inotifyTools
+          unetbootin
+          alsaUtils
+          lsof
+          darcs
+          # (haskellngPackages.ghcWithPackages (self : [
+             # self.xmonad
+             # self.xmonad-contrib
+             # self.xmonad-extras
+             # self.xmobar
+             # self.pandoc
+             # self.ncurses
+             # self.cabal-install
+             # haskell-pack deps
+             # self.stylish-haskell
+             # self.hasktags
+             # self.cabal2nix
+             # self.lens
+             # self.hdevtools
+             # self.zlib
+             # self.mtl
+             # self.HUnit
+             # self.QuickCheck
+             # self.hoogle
+          # ]))
+          rlwrap
+          fortune cowsay
+          ffmpeg
+          simplescreenrecorder
+          keymon
+          imagemagick
+          libreoffice
+          cups samba
+          telnet
+          # rhino
+          # nodejs nodePackages.npm nodePackages.jshint nodePackages.grunt-cli nodePackages.npm2nix nodePackages.bower2nix
+          ncurses
+          # x11vnc tightvnc
+          opam
         ];
       };
 
