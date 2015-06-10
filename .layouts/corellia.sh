723ece86078c07186b68a34a867ff33baa8d548b
Antoine R. Dumont <eniotna.t@gmail.com>
2015-06-10 10:51:28 +0200

    layout - Prepare to be used in udev rules

--- .layouts/corellia.sh
+++ .layouts/corellia.sh
@@ -1,8 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # VGA1 is the right screen
 # LVDS1 is the laptop screen
-
-disconnect=$1
 
 width=1920
 height=1080
@@ -12,14 +10,18 @@ res_screen1=1600x1200
 screen0=eDP1
 screen1=DP2
 
-if [ ! -z "$disconnect" ]; then
+function one_screen {
+    echo "one screen"
     xrandr --output $screen0 \
            --mode $res_screen0 \
            --pos 0x0 \
            --rotate normal \
            --output $screen1 \
            --off
-else
+}
+
+function two_screens {
+    echo "two screens"
     xrandr --output $screen0 \
            --mode $res_screen0 \
            --pos 0x0 \
@@ -28,4 +30,14 @@ else
            --mode $res_screen1 \
            --pos ${width}x0 \
            --rotate normal
+}
+
+set -x
+
+status_disconnected=$(xrandr | grep -i disconnected | grep -i $screen1)
+
+if [ ! -z "$status_disconnected" ]; then
+    one_screen
+else
+    two_screens
 fi
