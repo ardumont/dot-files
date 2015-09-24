95c1cf7601f9dc1a7dcf44cf2542d0adb1c4e33d
Antoine R. Dumont <eniotna.t@gmail.com>
2015-09-24 19:45:35 +0200

    NixOS - Keep the system packages

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -36,5 +36,112 @@
 
   # List packages installed in system profile. To search by name, run:
   # nix-env -qaP | grep wget
-  environment.systemPackages = with pkgs; [ hello ];
+  environment.systemPackages = with pkgs; [
+    python3
+    xorg_sys_opengl mesa_glu freeglut
+    cabal2nix
+    hexedit
+    bc
+    html2text
+    pciutils # lspci, etc...
+    nix-prefetch-scripts nix-repl nixops nox
+    psmisc # fuser
+    python python27Packages.screenkey
+    pidgin-with-plugins
+    androidsdk_4_4
+    wmname
+    feh
+    # steam steamChrootEnv # sudo init-steam-chrootenv mount-steam-chrootenv load-steam-chrootenv
+    xsane
+    jq
+    which
+    peco
+    nix-repl
+    gnome3_16.nautilus gnome3_16.gnome_settings_daemon
+    gnome3_16.eog pinta scrot
+    vlc x264 mplayer
+    gnome3_16.zenity
+    transmission_gtk
+    audacious
+    linuxPackages.virtualbox packer vagrant docker
+    evince fbreader mcomix
+    filezilla
+    git gitAndTools.tig gitAndTools.hub gitg meld libgit2
+    gnupg gnupg1 pinentry
+    pmount file
+    wget curl tree
+    gcc gnumake
+    bittorrentSync20
+    trayer
+    networkmanagerapplet
+    x11 xlibs.xmessage xlibs.xmodmap xdotool x11_ssh_askpass xscreensaver xlibs.xbacklight xlibs.xdpyinfo xlibs.xkill xlibs.xhost
+    libxml2
+    mosh
+    offlineimap notmuch
+    most
+    xclip xsel pass pwgen keychain
+    htop # powertop
+    tmux bind rxvt_unicode urxvt_perls
+    bash zsh ruby
+    bundler
+    zlib
+    firefoxWrapper conkeror #chromium
+    graphviz
+    nmap netcat wireshark
+    p7zip unrar zip unzip
+    acpi acpid acpitool
+    clojure leiningen jdk
+    gparted testdisk
+    binutils
+    pmutils
+    autojump
+    inotifyTools
+    unetbootin
+    alsaUtils
+    lsof
+    darcs
+    (haskellngPackages.ghcWithPackages (self : [
+       self.cabal-install
+       self.xmonad
+       self.xmonad-contrib
+       self.xmonad-extras
+       self.xmobar
+       self.pandoc
+       self.stack
+       # self.ncurses
+       # haskell-pack deps
+       self.stylish-haskell
+       self.structured-haskell-mode
+       self.hlint
+       self.hasktags
+       # self.cabal2nix
+       # self.lens
+       # self.hdevtools
+       # self.zlib
+       # self.mtl
+       # self.HUnit
+       # self.QuickCheck
+       # self.hoogle
+    ]))
+    rlwrap
+    fortune cowsay
+    ffmpeg
+    simplescreenrecorder
+    keymon
+    imagemagick
+    libreoffice
+    ### printing
+    cups samba
+    telnet
+    ncurses
+    ### javascript stack
+    # rhino
+    # nodejs nodePackages.npm nodePackages.jshint nodePackages.grunt-cli nodePackages.npm2nix nodePackages.bower2nix
+    ### communication
+    # x11vnc tightvnc
+    ### ocaml
+    opam
+    ### common-lisp
+    sbcl
+  ];
 }
