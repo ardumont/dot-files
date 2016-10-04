5be3b1bd1d2a97d942ff8994aab20a7aa486e806
Antoine R. Dumont <eniotna.t@gmail.com>
2016-10-04 18:56:18 +0200

    nixos - Add some packages

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -39,6 +39,8 @@
   # List packages installed in system profile. To search by name, run:
   # nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
+    qemu
+    sshfs
     python3
     # python34Packages.powerline
     powerline-fonts
