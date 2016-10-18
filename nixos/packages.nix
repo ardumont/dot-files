3d1eda332e7ab0367735d0eb7c44ce24ff066a15
Antoine R. Dumont <eniotna.t@gmail.com>
2016-10-18 19:50:59 +0200

    nixos: Clean up commented package overrides

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -11,27 +11,7 @@
     allowUnfree = true;
     allowBroken = true;
 
-  #   firefox = {
-  #     # enableAdobeFlash = true;
-  #     enableGoogleTalkPlugin = true;
-  #   };
-
-  #   chromium = {
-  #     enableGoogleTalkPlugin = true;
-  #     enablePepperFlash = true; # Chromium's non-NSAPI alternative to Adobe Flash
-  #     enablePepperPDF = true;
-  #   };
-
     packageOverrides = pkgs: {
-  #     # override the default pidgin with plugins (empty by default)
-  #     pidgin-with-plugins = pkgs.pidgin-with-plugins.override {
-  #       plugins = with pkgs; [ pidginotr skype4pidgin ];
-  #     };
-
-  #     # xmonad-with-packages = pkgs.xmonad-with-packages.override {
-  #     #   packages = with pkgs.haskellngPackages.ghcWithPackages; [ xmonad-contrib xmonad-extras ];
-  #     # };
-
        gnupg1compat = pkgs.gnupg1compat.override { gnupg = pkgs.gnupg21; };
     };
   };
