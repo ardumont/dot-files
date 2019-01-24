5b2716dbf05f51a9bd3ba3f66b5bc88f88911f48
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-01-24 10:56:21 +0100

    .surfingkeys: Add basic configuration

---
+++ .surfingkeys.js
@@ -0,0 +1,14 @@
+// Search settings
+addSearchAliasX('D', 'ddgH', 'https://duckduckgo.com/html/?q=', 's', 'https://duckduckgo.com/ac/?q=', function(response) {
+    var res = JSON.parse(response.text);
+    return res.map(function(r){
+        return r.phrase;
+    });
+});
+settings.defaultSearchEngine = 'D';
+
+// Hints settings
+Hints.characters = "asdfghjkl";
+// Hints.scrollKeys = "";
+settings.hintAlign = "left";
+settings.smoothScroll = false;
