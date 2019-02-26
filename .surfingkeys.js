e60f9ce1982222ddf5cfa823976600ab281616b9
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-02-26 11:19:09 +0100

    .surfingkeys: Try to improve slightly default keys

--- .surfingkeys.js
+++ .surfingkeys.js
@@ -1,5 +1,14 @@
+//https://raw.githubusercontent.com/ardumont/dot-files/master/.surfingkeys.js
+
+// follow link in current tab
 map('d', 'f'); unmap('f');
+// follow link in a new tab (do not open the tab yet)
 map('f', 'gf'); unmap('gf');
+// pin/unpin tab
+map('<Alt-p>', '<Alt-j>'); unmap('<Alt-p>');
+
+// interactively switch tab
+map('T', 'xb'); unmap('T');
 
 // Search settings
 addSearchAliasX('D', 'ddgH', 'https://duckduckgo.com/html/?q=', 's', 'https://duckduckgo.com/ac/?q=', function(response) {
@@ -15,3 +24,9 @@ Hints.characters = "asdfgqwertzxcvb";
 Hints.scrollKeys = "";
 settings.hintAlign = "left";
 settings.smoothScroll = false;
+
+// deactivate surfingkeys by default on those sites
+settings.blacklist = {
+    "https://github.com": 1,
+    "https://mail.google.com/mail": 1,
+}
