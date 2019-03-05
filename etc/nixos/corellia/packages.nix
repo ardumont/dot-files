119e06e98330561cdb3ed4da4ffbdf55c6e2c763
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-03-05 13:17:56 +0100

    nixos/corellia/packages: Update dependencies

--- etc/nixos/corellia/packages.nix
+++ etc/nixos/corellia/packages.nix
@@ -3,11 +3,12 @@
 {
   environment.systemPackages = with pkgs; [
     python3
+    mr
     python36
     python36Packages.virtualenvwrapper python36Packages.virtualenv
+    python36Packages.cffi  python36Packages.vcversioner
     # texLiveFull
     docker docker_compose
     openvpn networkmanager-openvpn
-    git
   ];
 }
