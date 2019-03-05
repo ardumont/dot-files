972ddd04b3c0fca271b5d46420ba200975aa8d8d
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-03-05 22:12:41 +0100

    nixos/dns: Reference some local more cache dns routing setup

--- etc/nixos/dns.nix
+++ etc/nixos/dns.nix
@@ -17,4 +17,18 @@ server=/150.168.192.in-addr.arpa/192.168.150.26
 server=/.vlan/10.8.9.1
 server=/9.8.10.in-addr.arpa/10.8.9.1
 '';
+
+  environment.etc."${path}/work.conf".text = ''
+server=/internal.softwareheritage.org/192.168.100.29
+server=/100.168.192.in-addr.arpa/192.168.100.29
+server=/101.168.192.in-addr.arpa/192.168.100.29
+server=/200.168.192.in-addr.arpa/192.168.100.29
+server=/201.168.192.in-addr.arpa/192.168.100.29
+server=/202.168.192.in-addr.arpa/192.168.100.29
+server=/203.168.192.in-addr.arpa/192.168.100.29
+server=/204.168.192.in-addr.arpa/192.168.100.29
+server=/205.168.192.in-addr.arpa/192.168.100.29
+server=/206.168.192.in-addr.arpa/192.168.100.29
+server=/207.168.192.in-addr.arpa/192.168.100.29
+'';
 }
