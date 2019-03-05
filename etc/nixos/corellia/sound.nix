ac6b26af507b56cf20d5c8f8cad0f54737bc2cd4
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-03-05 23:59:45 +0100

    nixos/corellia/sound: Activate sound

--- etc/nixos/corellia/sound.nix
+++ etc/nixos/corellia/sound.nix
@@ -1,12 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # disable pc speaker audio card
-  # blacklistedKernelModules = [ "snd_pcsp" ];
-
-  # boot.extraModprobeConfig = ''
-  #   options snd slots=snd-hda-intel
-  #   # disable first card and enable the second one
-  #   options snd_hda_intel enable=0,1
-  # '';
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
 }
