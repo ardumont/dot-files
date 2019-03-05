25297c5ec797a0429138110690f2e2dc908d413e
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-03-05 10:56:44 +0100

    nixos/services: Clean up unused services

--- etc/nixos/services.nix
+++ etc/nixos/services.nix
@@ -9,15 +9,9 @@
       enable = true;
       interval = "00 19 * * *"; # update db at 19h every day
     };
-
     openssh.enable = true;
     ntp.enable = true;
 
     nixosManual.showManual = true; # Add the NixOS Manual on virtual console 8
-
-    # postgresql = {
-    #   enable = true;
-    #   package = pkgs.postgresql94;
-    # };
   };
 }
