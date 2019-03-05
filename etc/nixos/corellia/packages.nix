27d490689b109cf6698b41cb52759828c413e13f
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-03-05 22:51:54 +0100

    nixos/corellia/packages: Update deps

--- etc/nixos/corellia/packages.nix
+++ etc/nixos/corellia/packages.nix
@@ -6,7 +6,9 @@
     mr
     python36
     python36Packages.virtualenvwrapper python36Packages.virtualenv
-    python36Packages.cffi  python36Packages.vcversioner
+    python36Packages.cffi python36Packages.vcversioner
+    python36Packages.pystemd python36Packages.systemd
+    postgresql pkgconfig
     # texLiveFull
     docker docker_compose
     openvpn
