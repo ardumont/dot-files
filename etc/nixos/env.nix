e6bfc385dd554095ac95a918c79c02d4dff60460
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-03-05 12:14:55 +0100

    nixos/env: Install some aliases
    For other users, having some basic is nice as well.

--- etc/nixos/env.nix
+++ etc/nixos/env.nix
@@ -1,7 +1,14 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
   environment.shellAliases = {
     ll = "ls -l";
+    gst = "git status";
+    gco = "git checkout";
+    gbr = "git branch";
+    gfa = "git fetch --all";
+    gf = "git fetch";
+    em = "emacsclient -nw";
+    emc = "emacsclient --create-frame";
   };
 }
