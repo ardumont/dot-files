dcd612f2406b427935f85287c841548fa6600c57
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-03-05 10:30:08 +0100

    nixos/common: Use generated consoleFont
    if that's the right font, it was nice

--- etc/nixos/common.nix
+++ etc/nixos/common.nix
@@ -34,7 +34,7 @@
 
   # Select internationalisation properties.
   i18n = {
-    consoleFont = "lat9w-16";
+    consoleFont = "Lat2-Terminus16";
     consoleKeyMap = "us";
     defaultLocale = "en_US.UTF-8";
     supportedLocales = [ "en_US.UTF-8/UTF-8" ];
