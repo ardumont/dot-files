ea5565123ab95593cfcd426f3c5dd73749d97ee0
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-03-05 10:29:30 +0100

    nixos/common: Use systemd-boot service
    Drop grub as a tryout for now

--- etc/nixos/common.nix
+++ etc/nixos/common.nix
@@ -27,18 +27,6 @@
         # NixOS (as the installation section recommends).
         efiSysMountPoint = "/boot";
       };
-      grub = {
-        # despite what the configuration.nix manpage seems to indicate,
-        # as of release 17.09, setting device to "nodev" will still call
-        # `grub-install` if efiSupport is true
-        # (the devices list is not used by the EFI grub install,
-        # but must be set to some value in order to pass an assert in grub.nix)
-        devices = [ "nodev" ];
-        efiSupport = true;  # use grub boot loader
-        enable = true;      # version 2
-        version = 2;        # activate memtest entry
-        memtest86.enable = true; # Activate the check on memory
-      };
     };
   };
 
