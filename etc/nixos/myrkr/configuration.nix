065c1be03382c35afc6da45c5a0f90a9185ff37b
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-04-19 17:16:38 +0200

    nixos/myrkr: Align with dagobah

--- etc/nixos/myrkr/configuration.nix
+++ etc/nixos/myrkr/configuration.nix
@@ -6,8 +6,29 @@
 
 {
   imports = [
-    ./hardware-configuration.nix
+    ./hardware-configuration.nix # Include the results of the hardware scan.
     ./sound.nix
     ../mounts.nix
+    ../xsession.nix
+    ../dns.nix
+    ../guix.nix
+    (import ../openvpn/default.nix {
+      inherit config;
+      inherit pkgs;
+      vpn_name = "lan";
+      device = "tun0";
+      cipher = "AES-128-CBC";
+      with_ta = true;
+      with_passfile = true;
+    })
+    (import ../openvpn/default.nix {
+      inherit config;
+      inherit pkgs;
+      vpn_name = "work";
+      device = "tun1";
+      cipher = "BF-CBC";
+      with_ta = false;
+      with_passfile = false;
+    })
   ];
 }
