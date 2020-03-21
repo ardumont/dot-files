cb28ecd67081ba983a87de79e835797027f2f399
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2020-03-21 16:52:21 +0100

    users: Migrate to new user definition

--- etc/nixos/users.nix
+++ etc/nixos/users.nix
@@ -11,41 +11,40 @@ in {
     defaultUserShell = "${pkgs.zsh}/bin/zsh";
 
     # Define a user account. # `dummy` password by default
-    extraUsers = [
-    {
-      description = "Antoine R. Dumont (@ardumont)";
-      name = "${defaultUser}";
-      group = "users";
-      uid = 1000;
-      createHome = true;
-      home = "/home/${defaultUser}";
-      # password = "${defaultPassword}";  # for the first time, make sure to not
-                                          # be locked out passwordless then
-                                          # change it manually
-      extraGroups = [ "wheel" "audio" "video" "vboxusers" "docker"
-                      "networkmanager" "dialout" "input" ];
-      useDefaultShell = true;
-      openssh.authorizedKeys.keys = [
-        "${builtins.readFile ./corellia/pubkey.nix}"
-        "${builtins.readFile ./dagobah/pubkey.nix}"
-        "${builtins.readFile ./myrkr/pubkey.nix}"
-        "${builtins.readFile ./alderaan/pubkey.nix}"
-      ];
-    }
+    users = {
+      tony = {
+        description = "Antoine R. Dumont (@ardumont)";
+        name = "${defaultUser}";
+        group = "users";
+        uid = 1000;
+        createHome = true;
+        home = "/home/${defaultUser}";
+        # password = "${defaultPassword}";  # for the first time, make sure to not
+        # be locked out passwordless then
+        # change it manually
+        extraGroups = [ "wheel" "audio" "video" "vboxusers" "docker"
+                        "networkmanager" "dialout" "input" ];
+        useDefaultShell = true;
+        openssh.authorizedKeys.keys = [
+          "${builtins.readFile ./corellia/pubkey.nix}"
+          "${builtins.readFile ./dagobah/pubkey.nix}"
+          "${builtins.readFile ./myrkr/pubkey.nix}"
+          "${builtins.readFile ./alderaan/pubkey.nix}"
+        ];
+      };
 
-    {
-      description = "Christelle B. Héritier";
-      name = "chris";
-      group = "users";
-      uid = 1001;
-      createHome = true;
-      home = "/home/chris";
-      # password = "${defaultPassword}";  # for the first time, make sure to not
-                                          # be locked out passwordless then
-                                          # change it manually
-      extraGroups = [ "audio" "video" "networkmanager" ];
-      useDefaultShell = true;
-    }
-    ];
+      chris = {
+          description = "Christelle B. Héritier";
+          group = "users";
+          uid = 1001;
+          createHome = true;
+          home = "/home/chris";
+          # password = "${defaultPassword}";  # for the first time, make sure to not
+          # be locked out passwordless then
+          # change it manually
+          extraGroups = [ "audio" "video" "networkmanager" ];
+          useDefaultShell = true;
+        };
+    };
   };
 }
