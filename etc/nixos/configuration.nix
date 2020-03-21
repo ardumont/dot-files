45a869061b5e16f42828eab0c622933884e2d106
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2020-03-21 16:52:11 +0100

    Pin nixos using niv

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -1,16 +1,27 @@
-{ config, pkgs, ... }:
+{ config, lib, ... }:
 
-let hostName = with builtins;
-  "${head (split "\n" (readFile /etc/nix-hostname))}";
-in rec {
-  imports = [
+let sources = import ./niv/sources.nix;
+in with {
+  overlay = _: pkgs: {
+    niv = import sources.niv {};    # use the sources :)
+  };
+};
+let sources = import ./niv/sources.nix;
+    pkgs = import sources.nixos {
+      overlays = [ overlay ];
+      inherit config;
+    };
+    hostName = with builtins;
+      "${head (split "\n" (readFile /etc/nix-hostname))}";
+in {
+  imports = map (m: import m { inherit config lib pkgs; }) [
     # Include the specifics of the machine
     "/etc/nixos/${hostName}/configuration.nix"
     # Include the common parts
     ./common.nix
     ./nix.nix
     ./services.nix
-    ./print.nix
+    # ./print.nix
     ./env.nix
     ./network.nix
     ./users.nix
