a5850ceb257ea25b361d9308f5907464a504996c
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2020-03-22 13:10:38 +0100

    ssh: Allow ssh client to have a nix-store linked .ssh/authorized_keys

--- etc/nixos/services.nix
+++ etc/nixos/services.nix
@@ -7,7 +7,14 @@
       enable = true;
       interval = "00 19 * * *"; # update db at 19h every day
     };
-    openssh.enable = true;
+    openssh = {
+      enable = true;
+    extraConfig = ''
+# To allow ~/.ssh/authorized_keys to be linked on /nix/store (world
+# readable) on clients...
+StrictModes no
+      '';
+    };
     ntp.enable = true;
 
     nixosManual.showManual = true; # Add the NixOS Manual on virtual console 8
